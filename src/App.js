ca66d1ddb1fc495429450be41c9a147eaf8b120a
Oscar Scheja <scheja.oscar@gmail.com>
2022-10-25 20:23:10 +0200

    lagt till api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+
+    (async function() {
+      let content = await fetch(`/api/TempApi`);
+
+      let message = content.status < 300 ? (await content.text()) : `Error=${content.status}, ${content.statusText}`;
+      
+      setData(message);
+
+    })();
+
+      // fetch(`/api/TempApi`)
+      // .then((response) => console.log(response.text()))
+      // .then((actualData) => console.log(actualData))
+      // .catch((error) => {
+      //   console.log(error.message);
+      // });
+
+  });
+
+  return <>
+      <div>
+        {data}
+      </div>
+    </>
 }
 
 export default App;
