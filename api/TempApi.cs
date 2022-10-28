b4e27d1f51f0b8c5261c7a1ef2835f8c212dc3d8
Oscar Scheja <scheja.oscar@gmail.com>
2022-10-28 12:17:15 +0200

    tagit bort connection mot table storage som test

--- api/TempApi.cs
+++ api/TempApi.cs
@@ -17,42 +17,42 @@ namespace Company.Function
         [FunctionName("TempApi")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            [Table("Temperatures", "1", Connection = "TemperaturesDatabaseConnection")] CloudTable table,
             ILogger log)
         {
             //Detta ska ligga som ett Attribute unde http.. i metoden ovan. Ändra namn på tabellen weatherData och connectionen
             //connection ska också in i local settings
 
-            //
+            //[Table("Temperatures", "1", Connection = "TemperaturesDatabaseConnection")] CloudTable table,
             
-            // log.LogInformation("C# HTTP trigger function processed a request.");
-
-            // var apiResponse = new ApiResponse(
-            //         11.2,
-            //         14.0
-            //     );
             
-            // return new OkObjectResult(apiResponse);
+            log.LogInformation("C# HTTP trigger function processed a request.");
 
-            var query = table.CreateQuery<TableData>();
-            query.TakeCount = 5;
-
-            
-            // Här höll vi på ocj dribblade med Ienumerable vs tolist. antar att inparametern i metoden var Ienumerable å inte CludTable då
-
-            var result = (await table.ExecuteQuerySegmentedAsync(query,null)).ToList();
-
-            if(result.Any())
-            {
-                var apiResponse = new ApiResponse(
-                    result.First().Temperature,
-                    result.Average(temp => temp.Temperature)
+            var apiResponse = new ApiResponse(
+                    11.2,
+                    14.0
                 );
+            
+            return new OkObjectResult(apiResponse);
 
-                return new OkObjectResult(apiResponse);
-            }
+            // var query = table.CreateQuery<TableData>();
+            // query.TakeCount = 5;
 
-            return new OkResult();
+            
+            // // Här höll vi på ocj dribblade med Ienumerable vs tolist. antar att inparametern i metoden var Ienumerable å inte CludTable då
+
+            // var result = (await table.ExecuteQuerySegmentedAsync(query,null)).ToList();
+
+            // if(result.Any())
+            // {
+            //     var apiResponse = new ApiResponse(
+            //         result.First().Temperature,
+            //         result.Average(temp => temp.Temperature)
+            //     );
+
+            //     return new OkObjectResult(apiResponse);
+            // }
+
+            // return new OkResult();
         }
     }
     public record ApiResponse(double CurrentTemperature, double AverageTemperature);
